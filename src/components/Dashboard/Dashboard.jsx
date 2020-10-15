b4e5cd1036b28b1b81ae0871e8a2aa8e4796a60a
Arkadiusz Kwiatkowski <58771611+arecki3000@users.noreply.github.com>
2020-10-16 00:47:08 +0200

    added chat list in dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,16 +1,63 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { Paper } from "@material-ui/core";
+import {
+  Paper,
+  Button,
+  Box,
+  List,
+  TextField,
+  ListItem,
+  Divider
+} from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import styles from "./styles";
+import firebase from "../../config/firebaseConfig";
 
-const Dashboard = ({ classes, uid }) => {
+const Dashboard = ({ classes, logOut, ...props }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        props.history.push("/login");
+      });
+  };
+  const chats = ["arek", "marek", "korda", "damian", "michal"];
   return (
     <Container component="main" maxWidth="md">
       <CssBaseline />
-      <Paper className={classes.root}>some text</Paper>
+      <Paper className={classes.root}>
+        <Box width={1 / 4} className={classes.chatsBox}>
+          <Box height="85%" style={{ height: "85%", overflow: "auto" }}>
+            <List>
+              {chats.map((chat) => {
+                return (
+                  <div>
+                    <ListItem button>{chat}</ListItem>
+                    <Divider />
+                  </div>
+                );
+              })}
+            </List>
+          </Box>
+          <Box height="20%">
+            <Button
+              className={classes.button}
+              fullWidth
+              variant="text"
+              color="secondary"
+              onClick={(e) => handleClick(e)}
+            >
+              Log Out
+            </Button>
+          </Box>
+        </Box>
+        <Box width="100%" className={classes.oldMsgContainer}>
+          <Box></Box>
+        </Box>
+      </Paper>
     </Container>
   );
 };
--- src/components/Dashboard/styles.js
+++ src/components/Dashboard/styles.js
@@ -1,10 +1,29 @@
 const styles = (theme) => ({
+  "@global": {
+    "*::-webkit-scrollbar": {
+      width: "0.4em"
+    },
+    "*::-webkit-scrollbar-track": {
+      "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.00)"
+    },
+    "*::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(0,0,0,.1)"
+      // outline: "1px solid slategrey"
+    }
+  },
   root: {
     display: "flex",
     marginTop: theme.spacing(10),
     height: theme.spacing(48),
     padding: theme.spacing(3)
-  }
+  },
+
+  chatsBox: {
+    width: "30%",
+    marginRight: theme.spacing(3),
+    visibilty: "hidden"
+  },
+  button: {}
 });
 
 export default styles;
