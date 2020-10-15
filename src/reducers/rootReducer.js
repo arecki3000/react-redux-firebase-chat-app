22afa6517d8e3e487ed8ee79ec5f018b2e3041bd
Arkadiusz Kwiatkowski <58771611+arecki3000@users.noreply.github.com>
2020-10-15 21:16:30 +0200

    add auth reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { firebaseReducer } from "react-redux-firebase";
 import { firestoreReducer } from "redux-firestore";
+import authReducer from "./authReducers";
 
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  auth: authReducer
 });
 
 export default rootReducer;
