f96af75eb287b7375840b2abca46a1dafcf0a298
Arkadiusz Kwiatkowski <58771611+arecki3000@users.noreply.github.com>
2020-10-15 13:19:32 +0200

    configured firebase, firestore and firebase auth

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1R_pI6Mwnxevr1-p8pv48S8HjybLtF5k",
+  authDomain: "react-redux-firebase-cha-ac636.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-cha-ac636.firebaseio.com",
+  projectId: "react-redux-firebase-cha-ac636",
+  storageBucket: "react-redux-firebase-cha-ac636.appspot.com",
+  messagingSenderId: "229146348097",
+  appId: "1:229146348097:web:aa0d833250d52134fa14c0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
