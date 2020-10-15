bf757573f349702645659e61c93798e4ac06a90d
Arkadiusz Kwiatkowski <58771611+arecki3000@users.noreply.github.com>
2020-10-15 21:12:29 +0200

    add authentication actions

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,5 +1,41 @@
-// export const signIn = (creds) => {};
+export const signIn = (creds) => {
+  return (dispatch, getState, { getFirebase }) => {
+    getFirebase()
+      .auth()
+      .signInWithEmailAndPassword(creds.email, creds.password)
+      .then(() => {
+        dispatch({ type: "LOG_IN" });
+      })
+      .catch((err) => {
+        dispatch({ type: "LOG_IN_ERROR", err });
+      });
+  };
+};
 
-// export const signUp = (creds) => {};
+export const signUp = (creds) => {
+  return (dispatch, getState, { getFirebase }) => {
+    getFirebase()
+      .auth()
+      .createUserInWithEmailAndPassword(creds.email, creds.password)
+      .then(() => {
+        dispatch({ type: "SIGN_IN" });
+      })
+      .catch((err) => {
+        dispatch({ type: "SIGN_IN_ERROR", err });
+      });
+  };
+};
 
-// export const signOut = (creds) => {};
+export const signOut = () => {
+  return (dispatch, getState, { getFirebase }) => {
+    getFirebase()
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({ type: "SIGN_OUT" });
+      })
+      .catch((err) => {
+        dispatch({ type: "SIGN_OUT_ERROR", err });
+      });
+  };
+};
