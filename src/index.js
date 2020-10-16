ca6edeab1f353d71b5802113e01a53592777adf8
Arkadiusz Kwiatkowski <58771611+arecki3000@users.noreply.github.com>
2020-10-16 14:45:47 +0200

    deleted all reducers, actions and firebase functions i'll do it agin only with docs

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import { createFirestoreInstance } from "redux-firestore";
-import { getFirebase, ReactReduxFirebaseProvider } from "react-redux-firebase";
-import firebase from "./config/firebaseConfig";
 import App from "./App";
-import rootReducer from "./reducers/rootReducer";
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk.withExtraArgument({ getFirebase }))
-);
-
-const rrfProps = {
-  firebase,
-  config: {},
-  dispatch: store.dispatch,
-  createFirestoreInstance
-};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ReactReduxFirebaseProvider {...rrfProps}>
-          <App />
-        </ReactReduxFirebaseProvider>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   rootElement
 );
